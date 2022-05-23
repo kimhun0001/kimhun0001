3c1c77c6f32891231e0f58bdfc536d65b5000fda
kimhun <kimhun0001@naver.com>
2022-05-23 23:29:17 +0900

    업로드

---
+++ Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int a = 3;
+        int a2 = 20;
+        int a3 = 4;
+
+        int[] b = {5,8,4,0,6,7,9};
+        String c = "abc";
+        char[] d = {'a', 'b', 'c'};
+        String[] e = {"abc", "efg"};
+        double f = 12.345;
+
+        System.out.println(solution(b));
+    }
+    public static int solution(int[] nums) {
+        int answer = 0;
+        return answer;
+    }
+}
