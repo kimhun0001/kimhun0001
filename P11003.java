373061f11ca4ec203d35679bf4142dd55ed2b2a9
kimhun <kimhun0001@naver.com>
2022-07-31 20:15:29 +0900

    220731 p8:15

--- P11003.java
+++ P11003.java
@@ -12,22 +12,28 @@ public class P11003 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         // 출력을 그때 그때 하는 것보다 버퍼에 넣고 한번에 출력하기 위해 BufferedWriter를 사용
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int L = Integer.parseInt(st.nextToken());
+
         st = new StringTokenizer(br.readLine());
         Deque<Node> mydeque = new LinkedList<>();
         for (int i = 0; i < N; i++) {
             int now = Integer.parseInt(st.nextToken());
+
             // 새로운 값이 들어 올 때마다 정렬하지 않고 현재 수보다 큰 값을 덱에서 제거함으로써 시간복잡도를 줄일 수 있음
             while (!mydeque.isEmpty() && mydeque.getLast().value > now) {
                 mydeque.removeLast();
             }
+            //새로운 값이 전보다 작다면 Add 한다.
             mydeque.addLast(new Node(now, i));
-            // 범위에서 벗어난 값은 덱에서 제거
+
+            // 범위에서 벗어난 값은 덱에서 제거 + L값보다 커진 범위를 앞에서 부터 삭제.
             if (mydeque.getFirst().index <= i - L) {
                 mydeque.removeFirst();
             }
+            //첫번째 value 값을 뽑아서 출력후 한칸 비움.
             bw.write(mydeque.getFirst().value + " ");
         }
         bw.flush();
---
+++ out/production/kimhun0001/README.md
@@ -0,0 +1,8 @@
+- 👋 내 이름은 김현 입니다.
+- 👀 게임 기획자 이며 1인 개발을 하고 있습니다. 
+- 🌱 한국에서 게임 회사 취업을 위해 준비중 입니다.
+- 💞️ JAVA 언어를 공부 했으며 , 유니티를 사용해 봤습니다.
+- 📫 My name is Hyun Kim.
+   I am a game designer and one person development.
+   I am preparing for a job at a game company in Korea.
+   I have studied JAVA language and tried using Unity.
